af9837cc83a130da1325b85b97c9c09199804cd2
Dalyclnl <dalyarmijoa@yahoo.com>
2022-11-30 19:40:25 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -1,140 +1,79 @@
 /* eslint-disable */
 
-window.onload = function() {
-  //write your code here
-  const pronuns = [
-    "I",
-    "you",
-    "he",
-    "she",
-    "it",
-    "we",
-    "they",
-    "me",
-    "you",
-    "him",
-    "her",
-    "it",
-    "us",
-    "them",
-    "mine",
-    "yours",
-    "his",
-    "hers",
-    "its",
-    "ours",
-    "theirs",
-    "this",
-    "that",
-    "these",
-    "those"
-  ];
+//write your code here
+const pronuns = [
+  "you",
+  "he",
+  "she",
+  "it",
+  "we",
+  "they",
+  "me",
+  "him",
+  "her",
+  "us",
+  "them",
+  "mine",
+  "ours",
+  "this",
+  "that",
+  "these"
+];
 
-  const adjs = [
-    "bright",
-    "cheap",
-    "clean",
-    "dark",
-    "dirty",
-    "dry",
-    "easy",
-    "empty",
-    "expensive",
-    "free",
-    "full",
-    "hard",
-    "heavy",
-    "hungry",
-    "light",
-    "soft",
-    "strong",
-    "thirsty",
-    "weak",
-    "wet",
-    "whole",
-    "bad",
-    "deep",
-    "far",
-    "fat",
-    "good",
-    "high",
-    "large",
-    "long",
-    "low",
-    "narrow",
-    "near",
-    "shallow",
-    "short",
-    "skinny",
-    "small",
-    "thick"
-  ];
+const adjs = [
+  "empty",
+  "free",
+  "full",
+  "soft",
+  "weak",
+  "wet",
+  "whole",
+  "bad",
+  "deep",
+  "far",
+  "good",
+  "high",
+  "long",
+  "low",
+  "near"
+];
 
-  let nouns = [
-    "Cotton",
-    "Ring",
-    "Year",
-    "Sand",
-    "Art",
-    "Seat",
-    "Bus",
-    "Sugar",
-    "Bank",
-    "Ship",
-    "Bicycle",
-    "Horse",
-    "Head",
-    "Box",
-    "Sock",
-    "Street",
-    "Heat",
-    "Bed",
-    "Way",
-    "Face",
-    "Folder",
-    "Cent",
-    "Sky",
-    "Circle",
-    "City",
-    "Class",
-    "Color",
-    "Food",
-    "Company",
-    "Copy",
-    "low",
-    "narrow",
-    "near",
-    "shallow",
-    "short",
-    "skinny",
-    "small",
-    "thick"
-  ];
-  const tlds = ["com", "cl", "dk", "nl", "net", "es", "br", "us", "uk"];
+let nouns = [
+  "Sock",
+  "Heat",
+  "Bed",
+  "Way",
+  "Cent",
+  "Sky",
+  "City",
+  "Color",
+  "Food",
+  "Copy",
+  "low",
+  "near",
+  "short",
+  "small",
+  "thick"
+];
+const tlds = ["com", "cl", "dk", "nl", "net", "es", "br", "us", "uk"];
 
-  function domainGenerator() {
-    let domains = [];
-    for (let i = 0; i < pronuns.length; i++) {
-      const pronoun = pronuns[i];
-      for (let j = 0; j < adjs.length; j++) {
-        const adj = adjs[j];
-        for (let k = 0; k < nouns.length; k++) {
-          const noun = nouns[k];
-          for (let l = 0; l < tlds.length; l++) {
-            const tld = tlds[l];
-            const domain = pronoun + adj + noun + "." + tld;
-            domains.push(domain);
-          }
+function domainGenerator() {
+  let domains = [];
+  for (let i = 0; i < pronuns.length; i++) {
+    const pronoun = pronuns[i];
+    for (let j = 0; j < adjs.length; j++) {
+      const adj = adjs[j];
+      for (let k = 0; k < nouns.length; k++) {
+        const noun = nouns[k];
+        for (let l = 0; l < tlds.length; l++) {
+          const tld = tlds[l];
+          const domain = pronoun + adj + noun + "." + tld;
+          domains.push(domain);
         }
       }
     }
-    return domains;
   }
-  function printDomains(domainsInput) {
-    for (let i = 0; i < domainsInput.length; i++) {
-      console.log(domainsInput[i]);
-    }
-  }
-};
+  return domains;
+}
 
-printDomains(domainGenerator());
+console.log(domainGenerator());
